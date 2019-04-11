87acbe3a5a77903c0f1e26161ee1b06bbe6519ca
Yaodong Sheng <yaodongsheng@dyn195164.cc.lehigh.edu>
2019-04-11 00:26:57 -0400

    try put

--- client.cc
+++ client.cc
@@ -4,11 +4,11 @@ using namespace std;
 
 int operation_number;
 
-float put_percent = 0;
+float put_percent = 1;
 
 float multiput_percent = 0;
 
-float get_percent = 1;
+float get_percent = 0;
 
 int thread_number = 8;
 

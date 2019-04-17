58e5a68beac4c817def9a7b99d5b59c52977bea6
Yaodong Sheng <yaodongsheng@Yaodongs-MBP.lan>
2019-04-16 22:24:58 -0400

    rht

--- runClient.sh
+++ runClient.sh
@@ -1,5 +1,7 @@
-./client 10000
-ssh rht1 "./client 10000"
-ssh rht2 "./client 10000"
-ssh rht3 "./client 10000"
-ssh rht4 "./client 10000"
+#!/bin/bash
+# $1 = opearation-number, $2 = put_percent $3 = get_percent
+./client $1
+ssh rht1 "./client $1 $2 $3" 
+ssh rht2 "./client $1 $2 $3"
+ssh rht3 "./client $1 $2 $3"
+ssh rht4 "./client $1 $2 $3"
